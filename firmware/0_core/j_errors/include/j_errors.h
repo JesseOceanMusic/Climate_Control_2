8fd987b365dab97ffd35d3fba80e664485f35448
JesseOceanMusic <narayanabox@gmail.com>
2025-06-23 23:35:14 +0300

    v.0.6

--- firmware/0_core/j_errors/include/j_errors.h
+++ firmware/0_core/j_errors/include/j_errors.h
@@ -4,18 +4,15 @@
   #define UNSIGNED_SHORT_BAD_RETURN_55555 55555                                 // чтобы было видно в отчете подозрительно симметричное число
 
   enum                                                                          // перечисление кодов ошибок
-                                                                                // raise_error(0) -> raise_error(ERROR_ID_UNDEFINED)
   {
     #define ERRORS_LIST(error_id, description) error_id,                        // X-MACROS
-    #include "j_errors_list.def"                                                // в папке "j_errors/include" он специально переименован
-                                                                                // в "j_errors_list_example.def", чтобы не было конфликта
-                                                                                // в основнов проекте
+    #include "j_errors_list.def"                                                // в папке "j_errors/include"
     #undef  ERRORS_LIST
     J_ERRORS_AMOUNT,
   };
 
   static const char ERRORS_HEADER_MESSAGE[] = "ERRORS:\nID/counter/description:\n";
-  extern bool unhandled_errors[J_ERRORS_AMOUNT];                                // ТОЛЬКО ДЛЯ j_errors_manager
+  extern bool unhandled_errors[J_ERRORS_AMOUNT];                                // ТОЛЬКО ДЛЯ j_errors_writer
   const char* get_error_description_ptr(unsigned short error_id);
   unsigned short get_error_counter(unsigned short error_id);
   void raise_error(unsigned short error_id);
